95414db74646103eb1ae7f0e525a86982f4dc73a
JoeAdr04 <joeladriantarqui@gmail.com>
2024-04-02 15:07:33 -0400

    actualizando todo lo AVANZADO 2

--- consume.py
+++
@@ -1,8 +0,0 @@
-from zeep import Client
-## https://www.dataaccess.com/webservicesserver/NumberConversion.wso.?WSDL
-
-client = Client(
-
-)
-result = client.service.NumberToWords(5)
-print (result)
